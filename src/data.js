03abc62b477de26ff72c24245b2ee1c5f73d3942
lfbianchini <lfbianchini@dons.usfca.edu>
2024-08-23 19:53:28 -0700

    link updates and apple icon

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ export const projects = [
     description:
       "This full-stack application provides secure file storage and sharing capabilities using React, Node.js, and AWS S3. It features real-time upload progress, temporary URL file sharing, and user authentication with PostgreSQL, demonstrating proficiency in modern web development practices.",
     image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://github.com/lfbianchini/dropbox-clone-web-app",
   },
   {
     title: "IDWR Office",
@@ -13,7 +13,7 @@ export const projects = [
     description:
       "This project management software, developed for investment company interns, streamlines workflow with automated LinkedIn verification and email notifications. It features data handling with Pydantic, parallel processing for enhanced performance, and an export feature for formatted Excel reports.",
     image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://github.com/rohanshah35/idwr-sleuth-tool",
   },
   {
     title: "RateMyProfessors Utility Tool",
@@ -21,7 +21,7 @@ export const projects = [
     description:
       "This Java application analyzes professor ratings using web scraping and natural language processing techniques. It provides comprehensive insights through sentiment analysis, grade averaging, and data visualization, showcasing skills in data processing and GUI development.",
     image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://github.com/lfbianchini/RMP-Search-Tool",
   },
   {
     title: "Archive Searcher and Organizer",
@@ -29,7 +29,7 @@ export const projects = [
     description:
       "This Java-based archival research tool for UC Berkeley's 'The Daily Californian' automates web scraping and PDF creation from historical newspaper archives. It integrates Azure Form Recognizer for OCR and keyword search functionality, significantly reducing manual research time.",
     image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
+    link: "https://github.com/lfbianchini/thedailycal-search-tool",
   },
 ];
 
