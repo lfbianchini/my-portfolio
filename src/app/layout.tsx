d1235d53f408d93d68ef9febae90ef38ef283cca
lfbianchini <lfbianchini@dons.usfca.edu>
2025-01-24 18:28:13 -0800

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { MantineProvider, createTheme } from "@mantine/core";
+import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import "./globals.css";
 
@@ -13,32 +13,6 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
-// const theme = createTheme({
-//   primaryColor: 'purple',
-//   defaultRadius: 'md',
-//   colors: {
-//     purple: [
-//       '#f3e5f5', 
-//       '#e1bee7', 
-//       '#ce93d8', 
-//       '#ba68c8', 
-//       '#ab47bc', 
-//       '#9c27b0', 
-//       '#8e24aa', 
-//       '#7b1fa2', 
-//       '#6a1b9a', 
-//       '#4a148c'
-//     ],
-//   },
-//   components: {
-//     Text: {
-//       defaultProps: {
-//         color: 'purple.6'
-//       }
-//     }
-//   }
-// });
 
 export const metadata: Metadata = {
   title: "Luca Bianchini's Portfolio",
