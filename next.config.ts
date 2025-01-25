0481b1a8a19630ed7e994836953be897931c8e89
lfbianchini <lfbianchini@dons.usfca.edu>
2025-01-24 18:42:21 -0800

    big fix

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Required for static export
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/my-portfolio/' : undefined, // Replace <repository-name> with your GitHub repo name
-  trailingSlash: true,
+  output: 'export',
   images: {
-    unoptimized: true, // Prevents Next.js from optimizing images
+    unoptimized: true,
   },
 };
 
