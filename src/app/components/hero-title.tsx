552314ce28e6058544e3a92ad7b1c4eceeac7566
lfbianchini <lfbianchini@dons.usfca.edu>
2025-03-19 15:27:09 -0700

    target blank

--- src/app/components/hero-title.tsx
+++ src/app/components/hero-title.tsx
@@ -35,6 +35,7 @@ export function HeroTitle() {
               leftSection={<IconDownload size={20} />}
               component="a"
               href="https://drive.google.com/file/d/1v28BIuKEpwPTgGKruHRuqN2y-UmzuN8u/view?usp=sharing"
+              target="_blank"
               download
             >
               View Resume
